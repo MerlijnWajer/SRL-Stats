43c447fee3eb19b0eff08b82a36f6b8262a26b1c
Merlijn Wajer <merlijn@wizzup.org>
2010-10-10 13:56:53 +0200

    Change to PSQL database.
    (MySQL can be used too with a small tweak)

--- sql.py
+++ sql.py
@@ -1,6 +1,10 @@
 import sqlalchemy
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///database.db', echo=True)
+
+from stats_credentials import dbu, dbpwd, dwh, dbp, dbname
+
+engine = create_engine("postgresql+psycopg2://%s:%s@%s:%s/%s" % (dbu, dbpwd, dwh,
+        dbp, dbname))
 
 from classes import User, Script, Variable, Commit, CommitVar, Base
 
--- stats.py
+++ stats.py
@@ -2,15 +2,20 @@
 
 from flup.server.fcgi import WSGIServer
 from jinja2 import Template
+from sql import *
 
 
-def wsgi(env, start_response):
+def stats(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain;charset=utf8')])
-#    return ["Ohai world"]
-    tmpl = Template('Hello {{ name }}!')
+#    tmpl = Template('Hello {{ name }}!')
 
-    return [str(tmpl.render({'name' : env['PATH_INFO']}))]
+#    route = env['PATH_INFO']
+#    return [repr(route)]
+
+#    return [str(tmpl.render({'name' : env['PATH_INFO']}))]
+    return [repr(session.query(User).all())]
+#    return [repr(env)]
 
 if __name__ == '__main__':
-    WSGIServer(wsgi).run()
+    WSGIServer(stats).run()
 
