836d1c57857a36c6992cade933edc7c9cf62a5a6
Merlijn Wajer <merlijn@wizzup.org>
2010-10-10 23:19:56 +0200

    classes.py: Clean up and add documentation.

--- classes.py
+++ classes.py
@@ -8,6 +8,14 @@ Base = declarative_base()
 metadata = Base.metadata
 
 class User(Base):
+    """
+        The User object represents a user.
+        It has a name, password and date registered.
+        Associated with the user are 'scripts' and 'commits'.
+        
+        Scripts consists of the scripts owned by the user,
+        Commits consists of the commits submitted by the user.
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
@@ -16,6 +24,7 @@ class User(Base):
     registertime = Column(DateTime, default=func.now())
     # scripts = user owned scripts
     # commits = user owned commits
+
     def __init__(self, name, password):
         self.name = name
         self.password = password
@@ -24,6 +33,10 @@ class User(Base):
         return '<User(%s)>' % self.name
 
 class Script(Base):
+    """
+        The Script object represents a script.
+        It has a name, and an owner. The owner is a User instance.
+    """
     __tablename__ = 'scripts'
 
     id = Column(Integer, primary_key=True)
@@ -40,6 +53,13 @@ class Script(Base):
         return '<Script(%s : %s)>' % (self.name, self.owner)
 
 class Commit(Base):
+    """
+        The Commit object represents a single commit.
+        A single commit consists of a user (User instance),
+        a script (Script Instace), the time it adds and the time the
+        commit has was submitted. Associated with a commit are additional
+        variables. (Variable instances)
+    """
     __tablename__ = 'commits'
 
     id = Column(Integer, primary_key=True)
@@ -60,6 +80,11 @@ class Commit(Base):
 
 
 class Variable(Base):
+    """
+        The Variable object represents a Random Event or Script Variable.
+        It has a name and a boolean to determine whether it is a variable
+        or random event.
+    """
     __tablename__ = 'variables'
 
     id = Column(Integer, primary_key=True)
@@ -74,6 +99,11 @@ class Variable(Base):
         self.is_var = is_var
 
 class CommitVar(Base):
+    """
+        The CommitVar object represents a commit variable (value).
+        It is linked to a specific Commit and Variable.
+        It only holds an amount (of the specific variable)
+    """
     __tablename__ = 'commitvars'
 
     id = None # Works?
