4542b7ab3baad5708834ed55f5d763352e3adebc
Merlijn Wajer <merlijn@wizzup.org>
2010-10-10 23:29:20 +0200

    Stats.py: Template example.

--- stats.py
+++ stats.py
@@ -6,14 +6,22 @@ from sql import *
 
 
 def stats(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain;charset=utf8')])
-#    tmpl = Template('Hello {{ name }}!')
+    start_response('200 OK', [('Content-Type', 'text/html;charset=utf8')])
+    tmpl = Template('''<html><head></head><body>
+            <ul>
+            {% for user in users %}
+                <li> {{ user }} </li>
+            {% endfor %}
+            </ul>
+            </body>
+            </html>''')
 
 #    route = env['PATH_INFO']
 #    return [repr(route)]
 
-#    return [str(tmpl.render({'name' : env['PATH_INFO']}))]
-    return [repr(session.query(User).all())]
+#    return [repr(session.query(User).all())]
+    users = session.query(User).all()
+    return [str(tmpl.render({'users' : users}))]
 #    return [repr(env)]
 
 if __name__ == '__main__':
