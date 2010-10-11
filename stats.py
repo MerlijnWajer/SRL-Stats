38b0bf1adc6ce56d6ed39f9a84f60c2920daf686
Merlijn Wajer <merlijn@wizzup.org>
2010-10-12 00:10:36 +0200

    stats.py: Changes to main interface.
    Will probably change the interface again soon.
    Added WebTool and TODO.

--- stats.py
+++ stats.py
@@ -1,29 +1,40 @@
 #!/usr/bin/env python
 
+# Routes:
+# /graph for graphs? (filetype png?)
+# /user etc for user pages? etc
+
 from flup.server.fcgi import WSGIServer
-from jinja2 import Template
+from jinja2 import Environment, PackageLoader
 from sql import *
+from webtool import WebTool
 
 
 def stats(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html;charset=utf8')])
-    tmpl = Template('''<html><head></head><body>
-            <ul>
-            {% for user in users %}
-                <li> {{ user }} </li>
-            {% endfor %}
-            </ul>
-            </body>
-            </html>''')
 
-#    route = env['PATH_INFO']
-#    return [repr(route)]
+    return [wt.apply_rule(env['REQUEST_URI'])]
 
-#    return [repr(session.query(User).all())]
-    users = session.query(User).all()
-    return [str(tmpl.render({'users' : users}))]
-#    return [repr(env)]
+def general():
+    users = session.query(User).limit(10)
+    scripts = session.query(Script).limit(10)
+
+    tmpl = jinjaenv.get_template('main.html')
+    return str(tmpl.render({'users' : users, 'scripts' : scripts}))
+
+def user(userid):
+    return general()
+
+def graph(graphvar):
+    return repr(graphvar)
 
 if __name__ == '__main__':
+    jinjaenv = Environment(loader=PackageLoader('stats', 'templates'))
+    wt = WebTool()
+
+    import re
+    wt.add_rule(re.compile('/stats/user/([0-9]+)'), user, ['userid'])
+    wt.add_rule(re.compile('/stats/graph/(.+)'), graph, ['graphvar'])
+    wt.add_rule(re.compile('/stats'), general, [])
     WSGIServer(stats).run()
 
---
+++ webtool.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# REQUEST_METHOD = GET/POST
+# REQUEST_URI = /mai/linkoe
+# IP = REMOTE_ADDR
+
+class WebTool(object):
+    """
+        The WebTool class provides me with the required MiddleWare utilities.
+        Current functionality:
+            -   URL-Based function calling and argument parsing.
+    """
+
+    def __init__(self):
+        self.rules = {}
+        pass
+
+    def add_rule(self, rule, func, varnames):
+        if rule in self.rules:
+            raise WebToolException('Rule %s already exists' % rule)
+        self.rules[rule] = {'func': func, 'vars': varnames}
+
+    def add_rules(self, rules, funcs, varnames):
+        for rule, func, vnames in zip(rules, funcs, varnames):
+            self.add_rule(rule, func, vnames)
+    
+    def apply_rule(self, url):
+        for rule, fv in self.rules.iteritems():
+            m = rule.match(url)
+            if m:
+                l = [x for x in m.groups()]
+                if len(l) != len(fv['vars']):
+                    raise WebToolException('Matches does not equal variable \
+                            amount')
+                return fv['func'](**dict(zip(fv['vars'], l)))
+        return None
+
+def prt(userid=None):
+    print userid, scriptid
+
+if __name__ == '__main__':
+    import re
+    w = WebTool()
+    w.add_rule(re.compile('^/stats/user/([0-9]+)$'), prt, ['userid'])
+    print w.apply_rule('/stats/user/42')
