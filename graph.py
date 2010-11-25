9e813042f7d9c7e3a45c41456f783b56baca08f3
Merlijn Wajer <merlijn@wizzup.org>
2010-11-26 00:59:04 +0100

    Graphs: Add normalise()

--- graph.py
+++ graph.py
@@ -7,12 +7,23 @@ import matplotlib.pyplot as plt
 from pylab import *
 import numpy as np
 
+class GraphToolException(Exception):
+    pass
+
 class GraphTool(object):
     
     def __init__(self):
         pass
 
     def pie(self, frac, label, g_title, colours=None):
+        if len(frac) != len(label):
+            raise GraphToolException('len(frac) != len(label')
+
+        fraclabel = self.normalise(zip(frac, label))
+
+        frac, label = map(lambda x: x[0], fraclabel), map(lambda x: x[1],
+                fraclabel)
+
         fig = plt.figure()
 
         p = fig.add_subplot(111)
@@ -27,3 +38,13 @@ class GraphTool(object):
         r = s.getvalue()
         s.close()
         return r
+
+    def normalise(self, fraclabel):
+        if len(fraclabel) < 15:
+            return fraclabel
+
+        fraclabel = sorted(fraclabel)
+        fl = fraclabel[:14]
+        _sum = sum(map(lambda x: x[0], fraclabel[14:]))
+        fl.append((_sum, 'Others'))
+        return fl
