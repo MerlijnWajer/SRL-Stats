760727aa8022ea3ad540b7f5a0f1ce5dfffb155b
Merlijn Wajer <merlijn@wizzup.org>
2011-01-13 18:39:07 +0100

    SQL: Small cleanups.

--- sql.py
+++ sql.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine
 
 from stats_credentials import dbu, dbpwd, dwh, dbp, dbname
 
-engine = create_engine("postgresql+psycopg2://%s:%s@%s:%s/%s" % (dbu, dbpwd, dwh,
-        dbp, dbname))
+engine = create_engine("postgresql+psycopg2://%s:%s@%s:%s/%s" % (dbu, dbpwd,
+    dwh, dbp, dbname))
 
 # Pass echo=True to see all SQL queries
-# When using MySQL; pass charset=utf8&use_unicode=0
+# When using MySQL; pass charset=utf8&use_unicode=1
 
 from classes import User, Script, Variable, Commit, CommitVar, Base
 
