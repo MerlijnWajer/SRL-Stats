ba774ea27c7acb37b6552231d490e10b162caac9
Merlijn Wajer <merlijn@wizzup.org>
2011-03-12 21:43:08 +0100

    StatsL Snall fix for ranks.

--- stats.py
+++ stats.py
@@ -213,6 +213,7 @@ class ScheduledJob(object):
     def __call__(self, env, start_response):
         if time.time() - last_rank_time > 3600: # One hour
             update_user_ranking()
+            last_rank_time = time.time()
 
         return self.app(env, start_response)
 
